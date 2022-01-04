92608db4155c1ca78be5941b68e5314208b2e508
Juno <junonguyen27@gmail.com>
2022-01-04 11:39:54 -0600

    test commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+var apiCovid = function(){
+    var apiUrl = "https://api.covid19tracking.narrativa.com/api/2020-03-22/country/spain"
+
+    fetch(apiUrl)
+        .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+                    console.log(data);
+                })
+            }
+        })
+
+        .catch(function(){
+            alert("test")
+        });
+
+};
