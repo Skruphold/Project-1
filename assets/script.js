a89b2ae541f5d6759066cfe18dff1c316a3ea0ea
Skruphold <hunterco999@gmail.com>
2022-01-04 12:11:45 -0600

    ticket master api call for events

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+var tmAPIkey = "j6vHekkc5X8bANXHOmkGTl9eTugoLWGi"
+
+function getLocation () {
+    var tmURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + tmAPIkey;
+
+    fetch(tmURL)
+        .then(function(response){
+            if (response.ok){
+                response.json().then(function(data){
+                    console.log(data);
+                })
+            }
+        })
+
+        .catch(function(){
+            alert("test");
+        })
+}
+
+getLocation();
