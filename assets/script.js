ba5f54be3c90d9a30a54d31334eac169aae61906
Skruphold <hunterco999@gmail.com>
2022-01-05 09:58:37 -0600

    added comments for my code

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-console.log("hello world");
+// console.log("hello world"); tested script file
 
+// astablishing main variables
 var locationInput = $("#location");
 var eventInput = $("#eventInput");
 var concertsEl = $("#concert");
@@ -7,9 +8,11 @@ var sportsEl = $("#sports");
 var attractionsEl = $("#attractions");
 var submitBtn = $("#submitBtn");
 
-
+// Hunter's api key
 var tmAPIkey = "j6vHekkc5X8bANXHOmkGTl9eTugoLWGi"
 
+
+// Creating a event listener to the submit button that call's on our api call's and empty the inputed text.
 submitBtn.on("click", function(event) {
     event.preventDefault();
     var locationValue = locationInput.val().trim();
@@ -20,8 +23,7 @@ submitBtn.on("click", function(event) {
     // console.log(eventValue);
 })
 
-
-
+// made a function to call on an api based on the user inputed values that displays our response in the console. 
 function getLocation (locationValue, eventValue) {
     // var tmURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + tmAPIkey;
     var tmURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=" + eventValue + "&city=" + locationValue + "&apikey=" + tmAPIkey;
@@ -33,20 +35,22 @@ function getLocation (locationValue, eventValue) {
                     console.log(data);
                 })
                 }
+        // catching any errors
         })
-  .catch(function(){
-      alert("test");
+        .catch(function(){
+            alert("error");
         })
 }
+
+// Calling on getLocation
 getLocation();
 
 var apiCovid = function(){
-    var apiUrl = "https://api.covid19tracking.narrativa.com/api/2022-01-04/country/us/region/minnesota"
+    var apiUrl = "https://api.covid19tracking.narrativa.com/api/2022-01-04/country/US/region/Minnesota";
 
     fetch(apiUrl)
         .then(function(response){
             if(response.ok){
-
                 response.json().then(function(data){
                     console.log(data);
                 })
